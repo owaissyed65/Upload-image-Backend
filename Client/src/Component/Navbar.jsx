8f5bcc6ed2fc8cd7ca9939c9d7bbf32eb4e09c19
owaissyed65 <owaissyed65@gmail.com>
2022-10-10 22:12:32 +0500

    Fixed Bugs

--- Client/src/Component/Navbar.jsx
+++ Client/src/Component/Navbar.jsx
@@ -1,25 +1,27 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import images from '../Component/images/download.jpg'
 const Navbar = () => {
+    const Location = useLocation();
+
     return (
         <>
-        <header>
-        <nav>
-            <div className="logo">
-            <img src={images} alt="Error Loading..." />
-            </div>
-            <ul>
-                <li><Link to='/home'>Home</Link></li>
-                <li><Link to='/'>Page</Link></li>
-                <li><Link to='/signup'>Signup</Link></li>
-                <li><Link to='/login'>Login</Link></li>
-            </ul>
-            <div className="search">
-                <input type="text" placeholder='Search...'/>
-            </div>
-        </nav>
-        </header>
+            <header>
+                <nav>
+                    <div className="logo">
+                        <img src={images} alt="Error Loading..." />
+                    </div>
+                    <ul>
+                        <li><Link to='/home'>Home</Link></li>
+                        <li><Link to='/'>Page</Link></li>
+                        <li><Link to='/signup'>Signup</Link></li>
+                        <li><Link to='/login'>Login</Link></li>
+                    </ul>
+                    <div className="search">
+                        <input type="text" placeholder='Search...' />
+                    </div>
+                </nav>
+            </header>
         </>
     )
 }
