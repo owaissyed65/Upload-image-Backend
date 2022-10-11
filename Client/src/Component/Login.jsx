b73e2f70c47d78ee47fbe50f7295af9ebc40f955
owaissyed65 <owaissyed65@gmail.com>
2022-10-11 19:42:32 +0500

    AddinG Login Functionality

--- Client/src/Component/Login.jsx
+++ Client/src/Component/Login.jsx
@@ -3,17 +3,17 @@ import Context from '../UseContext/context';
 
 const Login = () => {
     const context = useContext(Context);
-    const { loginSubmit } = context;
+    const { loginSubmit, loginHandleChange, login } = context;
     return (
         <>
             <main>
                 <div className="shadow">
                     <div className="shadow-box-login">
                         <h1>Login Now</h1>
-                        <form onSubmit={loginSubmit}>
+                        <form onSubmit={loginSubmit} >
                             <div className="input-login ">
-                                <input type="email" placeholder='Email...' className='input-login-group' required={true} minLength={8} />
-                                <input type="password" placeholder='Password...' className='input-login-group' required={true} minLength={5} />
+                                <input type="email" placeholder='Email...' className='input-login-group' required={true} minLength={8} name='email' value={login.email} onChange={loginHandleChange} />
+                                <input type="password" placeholder='Password...' className='input-login-group' required={true} minLength={5} name='password' value={login.password} onChange={loginHandleChange} />
                             </div>
                             <div className="button">
                                 <button className='btn '>Login</button>
--- Client/src/UseContext/state.js
+++ Client/src/UseContext/state.js
@@ -1,15 +1,41 @@
-
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 import Context from "./context";
 
 const State = (props) => {
-    const loginSubmit = (e) =>{
+    // states
+    const [login, setlogin] = useState({ email: '', password: '' });
+
+    // variables
+    const url = "http://localhost:5000";
+    const navigate = useNavigate();
+    // Login HAndle CHange
+    const loginHandleChange = (e) => {
+        setlogin({ ...login, [e.target.name]: e.target.value });
+    }
+    // login SUbMit
+    const loginSubmit = async (e) => {
         e.preventDefault();
-        console.log("SUbmit")
+        const res = await fetch(`${url}/login`, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({email:login.email,password:login.password})
+        })
+        console.log(res)
+        const data = await res.json();
+        if (res.status===200){
+            localStorage.setItem('token',data.Authorization)
+            navigate('/home')
+        }
+        else {
+            window.alert("Invalid Credentials or user Not Found")
+        }
     }
     return (
         <div>
-            <Context.Provider value={{loginSubmit}}>
+            <Context.Provider value={{ loginSubmit,loginHandleChange,login }}>
                 {props.children}
             </Context.Provider>
         </div>
